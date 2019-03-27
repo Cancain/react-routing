22004e302407c09dcf8dd04eaefc6506a197fe16
Cancain <eriksson.tomas@gmail.com>
2019-03-27 09:48:00 +0100

    completed lecture 234

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import NewPost from '../../containers/Blog/NewPost/NewPost';
@@ -33,8 +33,9 @@ class Blog extends Component {
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
 
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
-                    <Route path="/posts" component={Posts} />
+                    <Route path='/new-post' component={NewPost} />
+                    <Route path='/posts' component={Posts} />
+                    <Redirect from='/' to='/posts' />
                 </Switch>
             </div>
         );
